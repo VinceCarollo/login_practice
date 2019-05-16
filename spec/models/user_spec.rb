38f6dc4f0af47938107c8900114b24f0a86bb82c
Vince Carollo <vincecarollo@gmail.com>
2019-05-16 14:19:14 -0600

    Add model spec for instance method user_exists in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :email_address }
+
+  describe 'instance methods' do
+    it '.exists?' do
+      user_1 = User.create!(name: 'vince', email_address: 'vincecarollo@gmail.com')
+      user_2 = User.new(name: 'vince', email_address: 'vincecarollo@gmail.com')
+      user_3 = User.new(name: 'vince', email_address: 'different@gmail.com')
+
+
+      expect(user_2.user_exists?).to be true
+      expect(user_3.user_exists?).to be false
+    end
+  end
 end
