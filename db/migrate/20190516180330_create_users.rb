a2712043153d63a0a113c90f17bff81e648d6f1e
Vince Carollo <vincecarollo@gmail.com>
2019-05-16 12:10:55 -0600

    Fix spelling error

--- db/migrate/20190516180330_create_users.rb
+++ db/migrate/20190516180330_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email_adress
+      t.string :email_address
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20190516180330) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email_adress"
+    t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
