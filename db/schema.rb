69b0cb02582dd21062e61616fe9a3c0a51e63fa5
Vince Carollo <vincecarollo@gmail.com>
2019-05-17 15:38:54 -0600

    Add password column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190516180330) do
+ActiveRecord::Schema.define(version: 20190517211141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190516180330) do
     t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
