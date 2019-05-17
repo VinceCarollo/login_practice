4e16d0772d31bd42acb48512e9d60f3f8e1d830f
Vince Carollo <vincecarollo@gmail.com>
2019-05-17 16:22:50 -0600

    Change login routes to be restful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :show, :create ]
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#authorize'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
