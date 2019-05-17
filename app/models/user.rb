cecfc4dbe428b70a1a27f7af9ab059c7de1bd699
Vince Carollo <vincecarollo@gmail.com>
2019-05-17 15:41:51 -0600

    Add class method to user model to authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email_address
+  validates_presence_of :name, :email_address, :password
+
+  def self.authenticate(email, password)
+    if User.exists?(email_address: email)
+      user = User.find_by(email_address: email)
+      user.password == password
+    else
+      false
+    end
+  end
 
   def user_exists?
     User.exists?(email_address: self.email_address.downcase)
