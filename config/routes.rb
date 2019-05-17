040aa1614283013d1a6918211a3cbb0f59fd352c
Vince Carollo <vincecarollo@gmail.com>
2019-05-17 17:11:12 -0600

    Add delete route to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :show, :create ]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
