09b2911a1aa5e249fd348f1c1061bea0ea6c7dc3
Vince Carollo <vincecarollo@gmail.com>
2019-05-17 15:38:42 -0600

    Add validation of password to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :password }
   it { should validate_presence_of :email_address }
 
   describe 'instance methods' do
     it '.exists?' do
-      user_1 = User.create!(name: 'vince', email_address: 'vincecarollo@gmail.com')
-      user_2 = User.new(name: 'vince', email_address: 'vincecarollo@gmail.com')
-      user_3 = User.new(name: 'vince', email_address: 'different@gmail.com')
+      user_1 = User.create!(name: 'vince', email_address: 'vincecarollo@gmail.com', password: '12345')
+      user_2 = User.new(name: 'vince', email_address: 'vincecarollo@gmail.com', password: '12345')
+      user_3 = User.new(name: 'vince', email_address: 'different@gmail.com', password: '12345')
 
 
       expect(user_2.user_exists?).to be true
